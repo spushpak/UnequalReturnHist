3e81e4500ea74eb42523ec4a130b21df2dd399fd
spushpak <sarkar.pushpak@gmail.com>
2017-06-16 02:37:54 -0700

    RD files generated

--- R/uneqhistCBF.R
+++ R/uneqhistCBF.R
@@ -19,12 +19,111 @@
 #' @author Pushpak Sarkar
 #' 
 #' 
-#' 
-#' 
-#' 
-#' 
+#' @rdname uneqhistCBF
 #' @export
-#' 
+
+uneqhistCBF <- function(dat.mat, saveReps=FALSE){
+  
+  # Convert the data to xts object (probably it's not necessary)
+  dat.xts <- xts(dat.mat[, -1], order.by = as.Date(dat.mat[, 1], "%m/%d/%Y"))
+  
+  # Keep a copy of original data
+  old.xts <- dat.xts 
+  
+  # Find which columns have 'NA' values; so these columns have shorter histtory
+  miss.hist.var <- colnames(dat.xts)[apply(dat.xts, 2, anyNA)]
+  
+  # Find which column has full history
+  long.hist.var <- setdiff(colnames(dat.xts), miss.hist.var)
+  
+  # Find how many observatins are missing for each short history columns
+  na_count <- apply(dat.xts, 2, function(x) sum(is.na(x)))
+  na_count <- as.matrix(na_count)
+  colnames(na_count) <- "num.miss"
+  
+  # Alternative way to find the full history column
+  #long.hist.var <- rownames(na_count)[na_count[, "num.miss"]==0]
+  
+  # Drop full history group from 'na_count' as 'NA' count for this group is zero
+  na_count <- na_count[miss.hist.var, "num.miss", drop=F]
+  
+  # Sort count of missing obs from smallest to largest
+  miss.itr <- sort(unique(na_count[,"num.miss"]))
+  
+  # Full length of the dataset
+  full.length <- nrow(dat.xts)
+  
+  new.dat <- dat.xts[, long.hist.var]
+  
+  # Start with the short history columns which have the smallest number of 
+  # observations missing i.e. this group is the longest short history group. Then 
+  # move on to the next shorter history group and so on.
+  for (i in miss.itr) {
+    short.hist.var <- rownames(na_count)[na_count[, "num.miss"]==i]
+    temp.dat <- dat.xts[, c(long.hist.var, short.hist.var), drop=F]
+    num.miss <- i
+    reg.dat <- fitValue(short.hist.var, long.hist.var, temp.dat, num.miss)
+    new.dat <- constructCBFData(reg.dat, new.dat, full.length)  
+  } 
+  
+  
+  ######################################################################
+  # Create the start index of each repeating block in the new dataset. Each repeating
+  # block has length equal to the row numbers of initial dataset.
+  row.grp <- seq(1, nrow(new.dat), by=full.length)
+  
+  # No. of repeatng blocks
+  num.block <- nrow(new.dat)/full.length
+  
+  # Create an empty list to contain risk measures of each repeating block
+  risk.metrics <- vector("list", num.block)
+  
+  # Create an empty matrix to hold the aggregate risk measures of the CBF data 
+  risk.vals <- matrix(0, nrow=6, ncol=length(miss.hist.var),
+                      dimnames=list(c("Skewness", "Kurtosis", "Mean", "Volatility", "Sharpe Ratio", "Expected Shortfall"), miss.hist.var))
+  
+  
+  # Repeat the above step for each repeating block
+  for (j in 0:(num.block-1)) {
+    risk.mat <- matrix(0, nrow=6, ncol=length(miss.hist.var),
+                       dimnames=list(c("Skewness", "Kurtosis", "Mean", "Volatility", "Sharpe Ratio", "Expected Shortfall"), miss.hist.var))
+    
+    start.indx <- j*full.length+1  
+    end.indx <- (j+1)*full.length
+    
+    # Extract the block based on start and end index    
+    block.dat <- new.dat[start.indx:end.indx, miss.hist.var, drop=F]
+    
+    # Compute risk measures for the extracted block
+    risk.mat["Skewness", ] <- moments::skewness(block.dat)
+    risk.mat["Kurtosis", ] <- moments::kurtosis(block.dat)
+    risk.mat["Mean", ] <- colMeans(block.dat)
+    risk.mat["Volatility", ] <- apply(block.dat, 2, sd)
+    risk.mat["Sharpe Ratio", ] <- apply(block.dat, 2, SharpeRatio, FUN="StdDev")
+    risk.mat["Expected Shortfall", ] <- apply(block.dat, 2, ES, p=0.95, method="historical")
+    
+    risk.metrics[[j+1]] <- risk.mat
+  }
+  
+  
+  # Constrcut the risk matrix for the aggregate CBF data
+  
+  risk.vals["Skewness", ] <- moments::skewness(new.dat[, miss.hist.var])
+  risk.vals["Kurtosis", ] <- moments::kurtosis(new.dat[, miss.hist.var])
+  risk.vals["Mean", ] <- colMeans(new.dat[, miss.hist.var])
+  risk.vals["Volatility", ] <- apply(new.dat[, miss.hist.var], 2, sd)
+  risk.vals["Sharpe Ratio", ] <- apply(new.dat[, miss.hist.var], 2, SharpeRatio, 
+                                       FUN="StdDev")
+  risk.vals["Expected Shortfall", ] <- apply(new.dat[, miss.hist.var], 2, ES, 
+                                             p=0.95, method="historical")
+  
+  
+  if(saveReps==TRUE) 
+    return(round(risk.metrics, digits = 3)) 
+  else 
+    return(round(risk.vals, digits = 3))
+}
+
 
 #### Do the regression and find fitted value for the missing returns
 fitValue <- function(short.hist.var, long.hist.var, ret.dat, num.miss){
@@ -66,7 +165,6 @@ fitValue <- function(short.hist.var, long.hist.var, ret.dat, num.miss){
 
 
 ### Constrcut the combined backfilled dataset based on the number of residuals
-
 constructCBFData <- function(reg.dat, new.dat, full.length){
   
   num.resid <- nrow(reg.dat$err.mat)
@@ -103,106 +201,3 @@ constructCBFData <- function(reg.dat, new.dat, full.length){
   return(new.dat)
 }
 
-
-
-uneqhistCBF <- function(dat.mat, saveReps=FALSE){
-  
-# Convert the data to xts object (probably it's not necessary)
-dat.xts <- xts(dat.mat[, -1], order.by = as.Date(dat.mat[, 1], "%m/%d/%Y"))
-
-# Keep a copy of original data
-old.xts <- dat.xts 
-
-# Find which columns have 'NA' values; so these columns have shorter histtory
-miss.hist.var <- colnames(dat.xts)[apply(dat.xts, 2, anyNA)]
-
-# Find which column has full history
-long.hist.var <- setdiff(colnames(dat.xts), miss.hist.var)
-
-# Find how many observatins are missing for each short history columns
-na_count <- apply(dat.xts, 2, function(x) sum(is.na(x)))
-na_count <- as.matrix(na_count)
-colnames(na_count) <- "num.miss"
-
-# Alternative way to find the full history column
-#long.hist.var <- rownames(na_count)[na_count[, "num.miss"]==0]
-
-# Drop full history group from 'na_count' as 'NA' count for this group is zero
-na_count <- na_count[miss.hist.var, "num.miss", drop=F]
-
-# Sort count of missing obs from smallest to largest
-miss.itr <- sort(unique(na_count[,"num.miss"]))
-
-# Full length of the dataset
-full.length <- nrow(dat.xts)
-
-new.dat <- dat.xts[, long.hist.var]
-
-# Start with the short history columns which have the smallest number of 
-# observations missing i.e. this group is the longest short history group. Then 
-# move on to the next shorter history group and so on.
-for (i in miss.itr) {
-  short.hist.var <- rownames(na_count)[na_count[, "num.miss"]==i]
-  temp.dat <- dat.xts[, c(long.hist.var, short.hist.var), drop=F]
-  num.miss <- i
-  reg.dat <- fitValue(short.hist.var, long.hist.var, temp.dat, num.miss)
-  new.dat <- constructCBFData(reg.dat, new.dat, full.length)  
-} 
-
-
-######################################################################
-# Create the start index of each repeating block in the new dataset. Each repeating
-# block has length equal to the row numbers of initial dataset.
-row.grp <- seq(1, nrow(new.dat), by=full.length)
-
-# No. of repeatng blocks
-num.block <- nrow(new.dat)/full.length
-
-# Create an empty list to contain risk measures of each repeating block
-risk.metrics <- vector("list", num.block)
-
-# Create an empty matrix to hold the aggregate risk measures of the CBF data 
-risk.vals <- matrix(0, nrow=6, ncol=length(miss.hist.var),
-                    dimnames=list(c("Skewness", "Kurtosis", "Mean", "Volatility", "Sharpe Ratio", "Expected Shortfall"), miss.hist.var))
-
-
-# Repeat the above step for each repeating block
-for (j in 0:(num.block-1)) {
-  risk.mat <- matrix(0, nrow=6, ncol=length(miss.hist.var),
-                       dimnames=list(c("Skewness", "Kurtosis", "Mean", "Volatility", "Sharpe Ratio", "Expected Shortfall"), miss.hist.var))
-
-  start.indx <- j*full.length+1  
-  end.indx <- (j+1)*full.length
-
-  # Extract the block based on start and end index    
-  block.dat <- new.dat[start.indx:end.indx, miss.hist.var, drop=F]
-
-  # Compute risk measures for the extracted block
-  risk.mat["Skewness", ] <- moments::skewness(block.dat)
-  risk.mat["Kurtosis", ] <- moments::kurtosis(block.dat)
-  risk.mat["Mean", ] <- colMeans(block.dat)
-  risk.mat["Volatility", ] <- apply(block.dat, 2, sd)
-  risk.mat["Sharpe Ratio", ] <- apply(block.dat, 2, SharpeRatio, FUN="StdDev")
-  risk.mat["Expected Shortfall", ] <- apply(block.dat, 2, ES, p=0.95, method="historical")
-
-  risk.metrics[[j+1]] <- risk.mat
-}
-
-
-# Constrcut the risk matrix for the aggregate CBF data
-
-risk.vals["Skewness", ] <- moments::skewness(new.dat[, miss.hist.var])
-risk.vals["Kurtosis", ] <- moments::kurtosis(new.dat[, miss.hist.var])
-risk.vals["Mean", ] <- colMeans(new.dat[, miss.hist.var])
-risk.vals["Volatility", ] <- apply(new.dat[, miss.hist.var], 2, sd)
-risk.vals["Sharpe Ratio", ] <- apply(new.dat[, miss.hist.var], 2, SharpeRatio, 
-                                     FUN="StdDev")
-risk.vals["Expected Shortfall", ] <- apply(new.dat[, miss.hist.var], 2, ES, 
-                                           p=0.95, method="historical")
-
-
-if(saveReps==TRUE) 
-  return(round(risk.metrics, digits = 3)) 
-else 
-  return(round(risk.vals, digits = 3))
-}
--- R/uneqhistPage.R
+++ R/uneqhistPage.R
@@ -17,12 +17,63 @@
 #' @author Pushpak Sarkar
 #' 
 #' 
-#' 
-#' 
-#' 
-#' 
+#' @rdname uneqhistPage
 #' @export
-#' 
+
+uneqhistPage <- function(dat.mat, saveReps=FALSE){
+  
+  # Convert the data to xts object (probably it's not necessary)
+  dat.xts <- xts(dat.mat[, -1], order.by = as.Date(dat.mat[, 1], "%m/%d/%Y"))
+  
+  # Keep a copy of original data - will be updated with fitted values for the
+  # missing portion
+  fitted.xts <- dat.xts 
+  
+  # Find which columns have 'NA' values; so these columns have shorter histtory
+  miss.hist.var <- colnames(dat.xts)[apply(dat.xts, 2, anyNA)]
+  
+  # Find which column has full history
+  long.hist.var <- setdiff(colnames(dat.xts), miss.hist.var)
+  
+  # Find how many observatins are missing for each short history columns
+  na_count <- apply(dat.xts, 2, function(x) sum(is.na(x)))
+  na_count <- as.matrix(na_count)
+  colnames(na_count) <- "num.miss"
+  
+  # Alternative way to find the full history column
+  #long.hist.var <- rownames(na_count)[na_count[, "num.miss"]==0]
+  
+  # Drop full history group from 'na_count' as 'NA' count for this group is zero
+  na_count <- na_count[miss.hist.var, "num.miss", drop=F]
+  
+  # Sort count of missing obs from smallest to largest
+  miss.itr <- sort(unique(na_count[,"num.miss"]))
+  
+  # Full length of the dataset
+  full.length <- nrow(dat.xts)
+  
+  resid.mat <- matrix(NA, nrow = full.length, ncol = length(miss.hist.var))
+  colnames(resid.mat) <- miss.hist.var
+  
+  # Start with the short history columns which have the smallest number of 
+  # observations missing i.e. this group is the longest short history group. Then 
+  # move on to the next shorter history group and so on.
+  for (i in miss.itr) {
+    short.hist.var <- rownames(na_count)[na_count[, "num.miss"]==i]
+    temp.dat <- dat.xts[, c(long.hist.var, short.hist.var), drop=F]
+    num.miss <- i
+    reg.dat <- fitValue(short.hist.var, long.hist.var, temp.dat, num.miss)
+    
+    fitted.xts[reg.dat$miss.val, short.hist.var] <- reg.dat$fitted.dat[reg.dat$miss.val, short.hist.var]
+    resid.mat[(length(reg.dat$miss.val)+1):nrow(resid.mat), short.hist.var] <- reg.dat$err.mat[, short.hist.var]
+  } ############# end of for loop (for each short history group)
+  
+  risk.list <- constructPageData(fitted.xts, resid.mat, miss.hist.var, miss.itr, 
+                                 na_count, sReps=saveReps)
+  
+  return(round(risk.list, digits = 3))
+  
+}
 
 
 #### Do the regression and find fitted value for the missing returns
@@ -146,59 +197,3 @@ constructPageData <- function(fitted.xts, resid.mat, miss.hist.var, miss.itr, na
 }
 
 
-
-
-uneqhistPage <- function(dat.mat, saveReps=FALSE){
-
-# Convert the data to xts object (probably it's not necessary)
-dat.xts <- xts(dat.mat[, -1], order.by = as.Date(dat.mat[, 1], "%m/%d/%Y"))
-
-# Keep a copy of original data - will be updated with fitted values for the
-# missing portion
-fitted.xts <- dat.xts 
-
-# Find which columns have 'NA' values; so these columns have shorter histtory
-miss.hist.var <- colnames(dat.xts)[apply(dat.xts, 2, anyNA)]
-
-# Find which column has full history
-long.hist.var <- setdiff(colnames(dat.xts), miss.hist.var)
-
-# Find how many observatins are missing for each short history columns
-na_count <- apply(dat.xts, 2, function(x) sum(is.na(x)))
-na_count <- as.matrix(na_count)
-colnames(na_count) <- "num.miss"
-
-# Alternative way to find the full history column
-#long.hist.var <- rownames(na_count)[na_count[, "num.miss"]==0]
-
-# Drop full history group from 'na_count' as 'NA' count for this group is zero
-na_count <- na_count[miss.hist.var, "num.miss", drop=F]
-
-# Sort count of missing obs from smallest to largest
-miss.itr <- sort(unique(na_count[,"num.miss"]))
-
-# Full length of the dataset
-full.length <- nrow(dat.xts)
-
-resid.mat <- matrix(NA, nrow = full.length, ncol = length(miss.hist.var))
-colnames(resid.mat) <- miss.hist.var
-
-# Start with the short history columns which have the smallest number of 
-# observations missing i.e. this group is the longest short history group. Then 
-# move on to the next shorter history group and so on.
-for (i in miss.itr) {
-  short.hist.var <- rownames(na_count)[na_count[, "num.miss"]==i]
-  temp.dat <- dat.xts[, c(long.hist.var, short.hist.var), drop=F]
-  num.miss <- i
-  reg.dat <- fitValue(short.hist.var, long.hist.var, temp.dat, num.miss)
-
-  fitted.xts[reg.dat$miss.val, short.hist.var] <- reg.dat$fitted.dat[reg.dat$miss.val, short.hist.var]
-  resid.mat[(length(reg.dat$miss.val)+1):nrow(resid.mat), short.hist.var] <- reg.dat$err.mat[, short.hist.var]
-} ############# end of for loop (for each short history group)
-
-risk.list <- constructPageData(fitted.xts, resid.mat, miss.hist.var, miss.itr, 
-                               na_count, sReps=saveReps)
-
-return(round(risk.list, digits = 3))
-
-}
